e3504772ec793ca2da577f714dfa61ac64aa4e31
VikJ13 <jhavikrant7@gmail.com>
2026-01-19 18:23:51 +0530

    Add Terraform outputs for visibility

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "resource_group_name" {
+  description = "Name of the resource group"
+  value       = azurerm_resource_group.rg.name
+}
+
+output "vnet_name" {
+  description = "Name of the virtual network"
+  value       = azurerm_virtual_network.vnet.name
+}
+
+output "web_subnet_id" {
+  description = "ID of the web subnet"
+  value       = azurerm_subnet.web_subnet.id
+}
